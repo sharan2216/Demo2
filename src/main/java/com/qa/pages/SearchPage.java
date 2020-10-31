a2876b2a2fc67bb187c77348a73a0891b074bea5
sharan2216 <66839251+sharan2216@users.noreply.github.com>
2020-10-31 15:43:06 +0530

    Update SearchPage.java

--- src/main/java/com/qa/pages/SearchPage.java
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -4,6 +4,11 @@ public class SearchPage {
 	public void SearchPage()
 	{
 		System.out.println("search page added");
-	}
 
+	}
+public void AdvancedSearchPage()
+	{
+		System.out.println("Advancedsearch page added");
+
+	}
 }
